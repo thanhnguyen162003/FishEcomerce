d5398964b1a27bcbfefa61456050b1d5ce04ac27
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2024-10-10 02:43:01 +0700

    fix

--- src/Infrastructure/Repositories/CustomerRepository.cs
+++ src/Infrastructure/Repositories/CustomerRepository.cs
@@ -26,5 +26,33 @@ namespace Infrastructure.Repositories
             await _context.Customers.AddAsync(customer);
             await _context.SaveChangesAsync();
         }
+
+        //
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer?> GetByIdAsync(Guid id)
+        {
+            return await _context.Customers.FindAsync(id);
+        }
+
+
+        public async Task UpdateAsync(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var customer = await GetByIdAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
