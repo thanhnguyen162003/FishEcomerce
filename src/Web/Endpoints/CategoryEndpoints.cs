96d0d29e3edc156ef028049e672a23aef09e7f88
Minh Khang <khangminh530@gmail.com>
2024-11-08 13:50:48 +0700

    push everything

--- src/Web/Endpoints/CategoryEndpoints.cs
+++ src/Web/Endpoints/CategoryEndpoints.cs
@@ -1,4 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using Application.Categories.Commands.CreateCategory;
+using Application.Categories.Commands.DeleteCategory;
+using Application.Categories.Commands.UpdateCategory;
+using Application.Categories.Queries;
+using Application.Common.Models;
+using Application.Common.Models.CategoryModels;
+using Application.Common.Utils;
 using Carter;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Web.Endpoints;
 
@@ -7,5 +18,56 @@ public class CategoryEndpoints : ICarterModule
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("api/v1/category");
+        group.MapPost("", CreateCategory).WithName(nameof(CreateCategory));
+        group.MapPatch("{categoryId}", UpdateCategory).WithName(nameof(UpdateCategory));
+        group.MapDelete("{categoryId}", DeleteCategory).WithName(nameof(DeleteCategory));
+        group.MapGet("", GetCategories);
+    }
+    
+    private async Task<IResult> CreateCategory(ISender sender, [FromBody, Required] CategoryCreateModel CategoryModel, ValidationHelper<CategoryCreateModel> validationHelper)
+    {
+        var (isValid, response) = await validationHelper.ValidateAsync(CategoryModel);
+        if (!isValid)
+        {
+            return Results.BadRequest(response);
+        }
+        
+        var result = await sender.Send(new CreateCategoryCommand{CategoryCreateModel = CategoryModel});
+        return result.Status == HttpStatusCode.OK ? Results.Ok(result) : Results.BadRequest(result);
+    }
+    
+    private async Task<IResult> UpdateCategory(ISender sender, [FromBody, Required] CategoryUpdateModel tankCategoryModel,[Required] Guid tankcategoryId, ValidationHelper<CategoryUpdateModel> validationHelper)
+    {
+        var (isValid, response) = await validationHelper.ValidateAsync(tankCategoryModel);
+        if (!isValid)
+        {
+            return Results.BadRequest(response);
+        }
+        
+        var result = await sender.Send(new UpdateCategoryCommand{CategoryId = tankcategoryId, CategoryUpdateModel = tankCategoryModel});
+        return result.Status == HttpStatusCode.OK ? Results.Ok(result) : Results.BadRequest(result);
+    }
+
+    private async Task<IResult> DeleteCategory(ISender sender, [Required] Guid categoryId)
+    {
+        var result = await sender.Send(new DeleteCategoryCommand{CategoryId = categoryId});
+        return result.Status == HttpStatusCode.OK ? Results.Ok(result) : Results.BadRequest(result);
+    }
+    
+    private async Task<IResult> GetCategories(ISender sender, [AsParameters] CategoryQueryFilter filter, HttpContext httpContext)
+    {
+        var result = await sender.Send(new GetCategoriesQuery(){QueryFilter = filter});
+        
+        var metadata = new Metadata
+        {
+            TotalCount = result.TotalCount,
+            PageSize = result.PageSize,
+            CurrentPage = result.CurrentPage,
+            TotalPages = result.TotalPages
+        };
+        
+        httpContext.Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(metadata));
+        
+        return JsonHelper.Json(result);
     }
 }
